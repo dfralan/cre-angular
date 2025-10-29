7aac524e20800b9e684560a249b6b58d3e642ebd
dfralan <dfralan@gmail.com>
2025-10-29 11:03:38 -0300

    fixed ENV Variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://cre-angular.onrender.com/api'
+  apiUrl: 'https://cre-angular.onrender.com/api' // Use your actual production API URL
 };
